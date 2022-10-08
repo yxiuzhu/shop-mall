f8e9dd54692d72cbfb0c45f564f241e88073bd5d
y_xiuzhu <y_xiuzhu@163.com>
2022-10-09 00:06:16 +0800

    polish: 【交易结算】组件

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,6 +6,7 @@ import Register from '@/pages/Register'
 import Detail from '@/pages/Detail'
 import AddCartSuccess from '@/pages/AddCartSuccess'
 import ShopCart from '@/pages/ShopCart'
+import Trade from '@/pages/Trade'
 
 export default [
   {
@@ -13,6 +14,7 @@ export default [
     component: Home,
     meta: { show: true },
   },
+  // 搜索页
   {
     // 如何让params可传可不传————加上?
     path: '/search/:keyword?',
@@ -24,30 +26,41 @@ export default [
       return { keyword: $route.params.keyword }
     },
   },
+  // 登录页面
   {
     path: '/login',
     component: Login,
     meta: { show: false },
   },
+  // 注册页面
   {
     path: '/register',
     component: Register,
     meta: { show: false },
   },
+  // 商品详情页
   {
     path: '/detail/:skuid',
     component: Detail,
     meta: { show: true },
   },
+  // 添加购物车成功
   {
     path: '/addcartsuccess',
     name: 'addcartsuccess',
     component: AddCartSuccess,
     meta: { show: true },
   },
+  // 购物车详情
   {
     path: '/shopcart',
     component: ShopCart,
+    meta: { show: true },
+  },
+  // 结算页面
+  {
+    path: '/trade',
+    component: Trade,
     meta: { show: true },
   },
   // 重定向：设置默认访问的路由地址
